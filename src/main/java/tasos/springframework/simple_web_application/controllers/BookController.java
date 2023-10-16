f50b53c095ca9d8da28708253b5930ad8c32cdeb
Tasos Antoniou <tasos.an.antoniou@gmail.com>
2023-10-16 22:51:15 +0300

    Implemented Simple Spring MVC Controller

---
+++ src/main/java/tasos/springframework/simple_web_application/controllers/BookController.java
@@ -0,0 +1,23 @@
+package tasos.springframework.simple_web_application.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tasos.springframework.simple_web_application.repositories.BookRepository;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
